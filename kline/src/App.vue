b539a1a3a667cd145c76a82ffdd5ff5fbaaa34f3
齐凯 <wb_qikai@zts.com.cn>
2021-08-02 15:08:34 +0800

    fix:k线示例修改

--- kline/src/App.vue
+++ kline/src/App.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="root">
-        <sxQuotePlaceHolder v-model="show" width="25%" height="300" :ticker="ticker" />
         <div class="btns">
             <button @click="show=!show">显示隐藏</button>
-            <input type="text" ref="ticker" />
+            <input type="text" ref="ticker" placeholder="请输入证券代码 例如 : 000001.SZ" />
             <button @click="ticker=$refs.ticker.value">切换证券</button>
         </div>
+        <p>width: {{KlineWidth}}</p>
+        <p>height: {{KlineHeight}}</p>
+        <p>当前显示证券:{{ticker}}</p>
+        <p>当前显示状态:{{show}}</p>
+        <sxQuotePlaceHolder ref="kline" v-model="show" width="25%" height="300" :ticker="ticker" />
     </div>
 </template>
 <script>
@@ -14,13 +18,25 @@ export default {
     data() {
         return {
             show: true,
-            ticker: "000001.SZ"
+            ticker: "000001.SH",
+            KlineWidth: "",
+            KlineHeight: ""
         }
     },
-    created() {
-
+    mounted() {
+        this.KlineWidth = this.$refs.kline.style.width;
+        this.KlineHeight = this.$refs.kline.style.height;
     }
 };
 </script>
 <style scoped>
+.root {
+    padding: 36px;
+}
+p {
+    color: #fff;
+}
+.btns > input[type="text"] {
+    width: 200px;
+}
 </style>
